cfcc6fbcb8abc216a571d2f3df401b97c78fe5ac
1254126619 <1254126619@qq.com>
2022-05-08 12:17:26 +0800

    first commit

---
+++ 02.lua
@@ -0,0 +1,59 @@
+-- 定义变量
+-- local num = 100
+-- print(num)
+
+
+-- 定义方法
+-- function sayHello()
+--     print("hello run")
+-- end
+-- sayHello()
+
+
+--  传递参数
+-- function max(a,b)
+--     if a>b then
+--         return a
+--     else
+--         return b
+--     end
+-- end
+
+-- print(max(2,3))
+
+
+-- 循环
+-- for var=1,100 do
+--     print(var)
+-- end
+
+-- 表
+-- Config = {inside="inside",inside2="inside2"}
+-- Config.words = "Hello"
+-- Config.num = 100
+-- Config["name"]="zhangsan"
+
+-- print(Config.words)
+-- print(Config.num)
+-- print(Config.name)
+-- print(Config.inside)
+-- print(Config.inside2)
+-- -- 表遍历
+-- for key, value in pairs(Config) do
+--     print(key,value)
+-- end
+
+-- 1       val3
+-- Key2    val2
+-- Key1    val1
+-- tab = {Key1="val1",Key2="val2","val3"}
+-- for key, value in pairs(tab) do
+--     print(key,value)
+-- end
+
+
+-- nil 比较  nil比较要加上双引号
+-- print(type(X))
+-- print(type(X)==nil)
+-- print(type(X)=="nil")
+
